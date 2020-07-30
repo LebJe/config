1e0353f111476498f4e853f40053df9ec0614ff7
LebJe <51171427+LebJe@users.noreply.github.com>
2020-07-29 21:14:12 -0400

    Commit.

--- Temp/Sources/Temp/main.swift
+++ Temp/Sources/Temp/main.swift
@@ -4,16 +4,12 @@ import SwiftShell
 func getLinuxCPUTemp() {
 	let strTemp = (try? String(contentsOf: URL(fileURLWithPath: "/sys/class/thermal/thermal_zone0/temp"))) ?? ""
 
-	let temp = (((Double(strTemp) ?? 0.0) / 1000.0) * 1.8) + 32
-
-	print(temp)
+	print((((Double(strTemp) ?? 0.0) / 1000.0) * 1.8) + 32)
 }
 
 func getCPUTemp() {
 	#if os(macOS)
-		let url = Bundle.module.url(forResource: "osx-cpu-temp", withExtension: nil)!.path
-		let res = run(url, "-F")
-		print(res.stdout)
+		print(run(Bundle.module.url(forResource: "osx-cpu-temp", withExtension: nil)!.path, "-F").stdout)
 	#else
 		getLinuxCPUTemp()
 	#endif
