eda77e25ba13cd862fca7f7ea283dbca4ead0047
LebJe <51171427+LebJe@users.noreply.github.com>
2020-08-29 15:12:31 -0700

    Commit.

--- setupShell.zsh
+++ setupShell.zsh
@@ -3,3 +3,9 @@ brew install zsh
 sudo sh -c "echo $(which zsh) >> /etc/shells"
 echo "Changing shells, you will be prompted for your password..."
 chsh -s $(which zsh)
+
+# Authenticate using Touch ID for `sudo`.
+
+sudo su -
+
+echo "auth       sufficient     pam_tid.so\n$(cat /etc/pam.d/sudo)" > /etc/pam.d/sudo
