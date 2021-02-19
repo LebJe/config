8d8c3b381003f1e2eca755487746218b299c3f0b
LebJe <51171427+LebJe@users.noreply.github.com>
2021-02-18 23:35:49 -0500

    Commit.

--- replaceIcons.zsh
+++ replaceIcons.zsh
@@ -1,15 +1,7 @@
 #!/bin/zsh
 
-swift replaceIcons.swift /Applications/Docker.app /Users/jefflebrun/config/Icons/2171818b5f62a0454953edaec89b1c27_Docker.icns
-swift replaceIcons.swift /Applications/Roblox.app /Users/jefflebrun/config/Icons/c4b360e85e3cc50b0f085e5fd738e281_Roblox_Alt_2.icns
-swift replaceIcons.swift /Applications/RobloxStudio.app /Users/jefflebrun/config/Icons/429dd9cf4acd5945eacd0ea5f5f90a0c_RobloxStudio_Alt.icns
-swift replaceIcons.swift /Applications/Visual\ Studio\ Code.app /Users/jefflebrun/config/Icons/f03f0fa23eb9ccfaa63ade898b763b54_Microsoft_Visual_Studio_Code_Alt_2.icns
-swift replaceIcons.swift /Applications/Wireshark.app /Users/jefflebrun/config/Icons/817bd2667adeb856fbb2d24957dcdcbc_1605425541008_Wireshark.icns
-swift replaceIcons.swift /Applications/Turbo\ Boost\ Switcher\ Pro.app /Users/jefflebrun/config/Icons/1f8eeb2a5f6069a0b53d1adb451df686_Turbo_Boost_Switcher_Pro.icns
-swift replaceIcons.swift /Applications/AdvancedRestClient.app /Users/jefflebrun/config/Icons/004452e5ddcaf45a09017276ceb73d13_1606498062011_Advanced_Rest_Client.icns
-swift replaceIcons.swift /Applications/Visual\ Studio\ Code\ -\ Insiders.app /Users/jefflebrun/config/Icons/84350af0b1becad4fe974c8f5f66d197_1606763360883_Visual\ Studio\ Code\ Insiders.icns
-swift replaceIcons.swift /Applications/Raycast.app /Users/jefflebrun/config/Icons/13f60983d18415b0dc0a4444d19d3001_1605527742660_raycast.icns
-swift replaceIcons.swift /Applications/Firefox.app /Users/jefflebrun/config/Icons/8d0343f61ae1c3103bb2d656bcfde463_Firefox.icns
+./replaceIcons.swift
+
 touch /Applications/Visual\ Studio\ Code.app
 touch /Applications/Docker.app
 touch /Applications/Roblox.app
