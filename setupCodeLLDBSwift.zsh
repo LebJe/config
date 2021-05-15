078b3fe38adf0b81808ea8c7bf031ab47f50d636
LebJe <51171427+LebJe@users.noreply.github.com>
2021-05-15 09:20:29 -0400

    Commit

---
+++ setupCodeLLDBSwift.zsh
@@ -0,0 +1,5 @@
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	cp CodeLLDB-Swift.json ~/.config/nvim/plugged/vimspector/gadgets/linux/.gadgets.d/
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	cp CodeLLDB-Swift.json ~/.config/nvim/plugged/vimspector/gadgets/macos/.gadgets.d/
+fi
