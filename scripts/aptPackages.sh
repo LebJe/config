3e930ea994fc4cd98902fd9ea5da6aab8cc16bcb
LebJe <51171427+LebJe@users.noreply.github.com>
2021-05-19 11:51:09 -0400

    Commit

--- scripts/aptPackages.sh
+++ scripts/aptPackages.sh
@@ -2,7 +2,7 @@
 
 echo "installing packages..."
 
-sudo apt install -y zsh exa neovim python3-neovim python 3 ripgrep fd-find
+sudo apt install -y zsh exa neovim python3-neovim python3 nodejs bat ripgrep fd-find
 
 sudo ln -s $(which fdfind) /usr/local/bin/fd
 sudo ln -s $(which batcat) /usr/local/bin/bat
