a0922dff7129e1b43931978ef13ea5fcf6521aad
LebJe <51171427+LebJe@users.noreply.github.com>
2021-05-26 13:28:03 -0400

    Commit.

--- scripts/aptPackages.sh
+++ scripts/aptPackages.sh
@@ -1,13 +1,17 @@
 #/bin/bash
 
-echo "installing packages..."
+PACKAGES="zsh exa gh neovim python3-neovim python3 nodejs npm bat ripgrep fd-find"
+
+echo "installing $PACKAGES ..."
 
 sudo add-apt-repository ppa:neovim-ppa/unstable
 
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 sudo apt update -q
 
-sudo apt install -y zsh exa neovim python3-neovim python3 nodejs npm bat ripgrep fd-find
-
+sudo apt install -yq $PACKAGES
 sudo ln -s $(which fdfind) /usr/local/bin/fd
 sudo ln -s $(which batcat) /usr/local/bin/bat
 
