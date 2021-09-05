4783f6561b36f7295f882faae0f0e2c693395cf3
LebJe <51171427+LebJe@users.noreply.github.com>
2021-09-05 15:06:49 -0400

    Add gitui install script

---
+++ scripts/downloadGitui.zsh
@@ -0,0 +1,3 @@
+wget -O gitui-linux-musl.tar.gz https://github.com/extrawurst/gitui/releases/download/v0.17/gitui-linux-musl.tar.gz
+tar -xzvf gitui-linux-musl.tar.gz
+echo "The gitui binary is at $(pwd)/gitui"
