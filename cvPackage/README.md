b5717e2b01de98d5b2feff487ebb8bb49f217af6
LebJe <51171427+LebJe@users.noreply.github.com>
2021-10-17 10:47:57 -0400

    Commit

--- cvPackage/README.md
+++
@@ -1,3 +0,0 @@
-# cv
-
-`cv` creates a [Vimspector](https://github.com/puremourning/vimspector.git) [configuration file](https://puremourning.github.io/vimspector/configuration.html).
--- scripts/setupCodeLLDBSwift.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	cp CodeLLDB-Swift.json ~/.local/share/nvim/site/pack/packer/opt/vimspector/gadgets/linux/.gadgets.d 
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	cp CodeLLDB-Swift.json ~/.local/share/nvim/site/pack/packer/opt/vimspector/gadgets/macos/.gadgets.d 
-fi
